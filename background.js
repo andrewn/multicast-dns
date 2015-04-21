2a6ddfd5608f7cd6082528282ac3004e7873f342
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-21 11:30:43 +0100

    Experiment with sending records

--- background.js
+++ background.js
@@ -10,16 +10,69 @@ mdns.on('warning', function (err) {
 
 mdns.on('response', function (response) {
   console.log('got a response packet:', response)
-})
+});
 
 mdns.on('query', function (query) {
   console.log('got a query packet:', query)
-})
+});
 
 // lets query for an A record for 'brunhilde.local'
-mdns.query({
-  questions: [{
-    name: 'brunhilde.local',
-    type: 'A'
-  }]
-})
+// mdns.query({
+//   questions: [{
+//     name: 'brunhilde.local',
+//     type: 'A'
+//   }]
+// })
+
+
+  // Advertising:
+  ptr = {
+    "name": "_ws._tcp.local",
+    "type": "PTR",
+    "data": "{channelName}[{peerId}]._ws._tcp.local"
+  };
+
+  srv = {
+    "name": "{channelName}[{peerId}]._ws._tcp.local",
+    "type": "SRV",
+    "data": {
+      "port": "{proxyPort}",
+      "target": "{generatedHostname}.local."
+    }
+  };
+
+  txt = {
+    "name": "{channelName}[{peerId}]._ws._tcp.local",
+    "type": "TXT",
+    "data": "path={peer.url}"
+  };
+
+  a = {
+    name: '{generatedHostname}.local.',
+    type: 'A',
+    data: '{SocketInfo.localAddress}'
+  };
+
+  mdns.respond({
+    answers: [ptr, srv, txt, a]
+  }, function () { console.log('response sent'); })
+
+/*
+
+  Search and connect:
+
+  1. A queries: _ws._tcp.local
+  mdns.query({
+    questions: [{
+      name: '_ws._tcp.local',
+      type: 'PTR'
+    }]
+  });
+
+  2. B Responds with PTR record
+
+  3. A receives PTRS
+
+  service name = 
+
+*/
